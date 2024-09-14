47051a0a0e9194435bf23646b9f115f48730243a
Praveen <saipraveenneelisetty27@gmail.com>
2024-09-14 22:29:32 +0530

    Setting up Backend by using Ansible

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -6,7 +6,7 @@ resource "aws_ssm_parameter" "vpc_id" {
 
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
-  type  = StringList
+  type  = "StringList"
   value = join(",", module.vpc.public_subnet_ids) # converting list to string list
 }
 
@@ -14,7 +14,7 @@ resource "aws_ssm_parameter" "public_subnet_ids" {
 # id1, id2 -> AWS SSM format
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
-  type  = StringList
+  type  = "StringList"
   value = join(",", module.vpc.private_subnet_ids) # converting list to string list
 }
 
